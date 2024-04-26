8ee9d1d1726ce1d068e99e07e0c0996cd942d183
ading2210 <ading2019@gmail.com>
2024-04-26 17:55:03 -0400

    cross compile for arm64 as well

--- build_repo.sh
+++ build_repo.sh
@@ -8,25 +8,28 @@ if [ "$DEBUG" ]; then
 fi
 
 supported_releases="bookworm unstable"
+architecture="amd64 arm64"
 base_path="$(realpath $(pwd))"
-repo_dir="$base_path/repo/"
-download_dir="$base_path/download/"
+repo_dir="$base_path/repo"
+download_dir="$base_path/download"
 
 rm -rf $repo_dir || true
 mkdir -p $repo_dir
 
 for release_name in $supported_releases; do
-  cd $base_path
+  for arch in $architecture; do
+    cd $base_path
 
-  pool_dir="$repo_dir/pool/main/$release_name"
-  dists_dir="$repo_dir/dists/$release_name/main/binary-amd64"
-  mkdir -p $dists_dir
-  mkdir -p $pool_dir
-  cp $download_dir/systemd_$release_name/*.deb $pool_dir
-  cp $download_dir/mesa_generic/*.deb $pool_dir
-  rm $pool_dir/*dbgsym*
+    pool_dir="$repo_dir/pool/main/$release_name"
+    dists_dir="$repo_dir/dists/$release_name/main/binary-$arch"
+    mkdir -p $dists_dir
+    mkdir -p $pool_dir
+    cp "$download_dir/systemd_${release_name}_${arch}"/*.deb $pool_dir
+    cp "$download_dir/mesa_generic_${arch}"/*.deb $pool_dir
+    rm $pool_dir/*dbgsym*
 
-  cd $repo_dir
-  dpkg-scanpackages --arch amd64 pool/main/$release_name > $dists_dir/Packages
-  cat $dists_dir/Packages | gzip -9 > $dists_dir/Packages.gz
+    cd $repo_dir
+    dpkg-scanpackages --arch $arch pool/main/$release_name > $dists_dir/Packages
+    cat $dists_dir/Packages | gzip -9 > $dists_dir/Packages.gz
+  done
 done
