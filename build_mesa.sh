0bf798568079123af33ee39f0eef9af1b121a840
ading2210 <ading2019@gmail.com>
2024-06-07 21:39:00 -0700

    rewrite build scripts and use a chroot instead of a vm

--- build_mesa.sh
+++ build_mesa.sh
@@ -1,49 +1,21 @@
 #!/bin/bash
 
-set -e
-if [ "$DEBUG" ]; then
-  set -x
-fi
+base_path="$(realpath $(dirname $0))"
+cd $base_path
+. ./common.sh
 
 print_help() {
-  echo "Usage: ./build_mesa.sh [arch]"
+  echo "Usage: ./build_mesa.sh distro_name release_name arch"
 }
 
-if [ "$EUID" -ne 0 ]; then 
-  print_help
-  echo "This script must be run as root."
-  exit 1
-fi
+assert_root
+assert_deps "debootstrap"
+assert_args "$3"
 
-if [ "$1" ]; then
-  arch="$1"
-else
-  arch="amd64"
-fi
+distro_name="$1"
+release_name="$2"
+arch="$3"
 
-release_name="bookworm"
-base_path="$(realpath $(dirname $0))"
-tmp_dir="/tmp/chromeos-systemd"
-qemu_dir="$base_path/qemu"
-disk_img="$qemu_dir/$release_name-$arch.img"
-
-echo "creating build directory"
-build_dir="$base_path/build"
-rm -rf $build_dir || true
-mkdir -p $build_dir
-cd $build_dir
-
-echo "downloading package source"
-source_dir="$build_dir/mesa-amber"
-git clone --depth=1 "https://salsa.debian.org/xorg-team/lib/mesa-amber.git" "$source_dir"
-
-echo "setting up sbuild virtual machine"
-mkdir -p $qemu_dir
-if [ ! -f "$disk_img" ]; then
-  sbuild-qemu-create --arch=$arch $release_name https://deb.debian.org/debian -o $disk_img
-fi
-
-echo "building package"
-cd $source_dir
-build_log="$base_path/build_$release_name_$arch.log"
-sbuild-qemu --image=$disk_img --ram=4096 --cpus=$(nproc --all) --arch=$arch --no-run-piuparts --no-run-lintian
+./build_package.sh $distro_name $release_name $arch \
+  source_type=git \
+  pkg_source=https://salsa.debian.org/xorg-team/lib/mesa-amber.git
---
+++ build_package_chroot.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+base_path="$(realpath $(dirname $0))"
+cd $base_path
+. ./common.sh
+
+print_help() {
+  echo "Usage: ./build_package_chroot.sh distro_name release_name arch"
+  echo "Valid named arguments (specify with 'key=value'):"
+  echo "  source_type - Package source type (either git or apt)"
+  echo "  pkg_source  - Package source location"
+  echo "  patches     - Patch files (relative to the repo dir)"
+}
+
+assert_root
+assert_args "$3"
+parse_args "$@"
+
+distro_name="$1"
+release_name="$2"
+arch="$3"
+
+source_type="${args['source_type']}"
+pkg_source="${args['pkg_source']}"
+patches="${args['patches']}"
+
+build_dir="$base_path/build"
+source_dir="$build_dir/pkg"
+
+if [ "$distro_name" = "debian" ]; then
+  repo_url="http://deb.debian.org/debian"
+else
+  echo "invalid distro name"
+  exit 1
+fi
+
+#install debian build tools
+apt-get install git devscripts quilt -y
+
+#create a directory to put the package source in
+rm -rf "$build_dir"
+mkdir -p "$build_dir"
+cd "$build_dir"
+
+#download the package source
+if [ "$source_type" = "git" ]; then
+  git clone --depth=1 "$pkg_source" "$source_dir"
+
+elif [ "$source_type" = "apt" ]; then
+  if ! grep -q "apt-src" "/etc/apt/sources.list"; then
+    echo "deb-src $repo_url $release_name main" >> /etc/apt/sources.list
+  fi
+
+  echo "downloading source package"
+  apt-get update
+  apt-get source "$pkg_source"
+  downloaded_dir=$(find "$build_dir" -mindepth 1 -maxdepth 1 -type d -printf '%f\n')
+  mv "$downloaded_dir" "$source_dir"
+
+else
+  echo "invalid source type"
+  exit 1
+fi
+
+#apply any needed patches
+cd "$source_dir"
+if [ "$patches" ]; then
+  for patch in "$patches"; do
+    patch_path="$base_path/$patch"
+    quilt import $patch_path
+  done
+  quilt push
+fi
+
+#install build deps
+mk-build-deps
+apt-get install -y ./*.deb
+
+#build the package
+export DEB_BUILD_OPTIONS=nocheck #skip tests
+dpkg-buildpackage -b -rfakeroot -us -uc -a$arch
