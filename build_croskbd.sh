56d3a9314283d4d81f4beaa84536ad9c2848f605
ading2210 <ading2019@gmail.com>
2026-02-15 17:17:25 -0800

    add croskbd to repo

---
+++ build_croskbd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+base_path="$(realpath $(dirname $0))"
+cd $base_path
+. ./common.sh
+
+print_help() {
+  echo "Usage: ./build_croskbd.sh distro_name release_name arch"
+}
+
+assert_root
+assert_deps "debootstrap"
+assert_args "$3"
+
+distro_name="$1"
+release_name="$2"
+arch="$3"
+
+./build_package.sh $distro_name $release_name $arch \
+  source_type=git \
+  pkg_source=https://github.com/ading2210/croskbd
